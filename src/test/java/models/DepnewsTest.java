0231927995ad1c2d40247f8c26a1c320759b33ea
Turi-byte <breldanmuturi001@gmail.com>
2019-09-24 22:12:22 +0300

    Add model tests

---
+++ src/test/java/models/DepnewsTest.java
@@ -0,0 +1,68 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepnewsTest {
+
+  @Test
+  public void compareTo() {
+  }
+
+  @Test
+  public void getId() {
+  }
+
+  @Test
+  public void setId() {
+  }
+
+  @Test
+  public void getContent() {
+  }
+
+  @Test
+  public void setContent() {
+  }
+
+  @Test
+  public void getWrittenBy() {
+  }
+
+  @Test
+  public void setWrittenBy() {
+  }
+
+  @Test
+  public void getRating() {
+  }
+
+  @Test
+  public void setRating() {
+  }
+
+  @Test
+  public void getDepartmentId() {
+  }
+
+  @Test
+  public void setDepartmentId() {
+  }
+
+  @Test
+  public void getCreatedat() {
+  }
+
+  @Test
+  public void setCreatedat() {
+  }
+
+  @Test
+  public void getFormattedCreatedAt() {
+  }
+
+  @Test
+  public void setFormattedCreatedAt() {
+  }
+}
