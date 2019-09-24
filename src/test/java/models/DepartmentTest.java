74c1f7eb00bc23ecf0306602bfbb3977f79dcfcb
Turi-byte <breldanmuturi001@gmail.com>
2019-09-24 22:09:36 +0300

    Add model tests

---
+++ src/test/java/models/DepartmentTest.java
@@ -0,0 +1,50 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepartmentTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void getId() {
+  }
+
+  @Test
+  public void setId() {
+  }
+
+  @Test
+  public void getName() {
+  }
+
+  @Test
+  public void setName() {
+  }
+
+  @Test
+  public void getDescription() {
+  }
+
+  @Test
+  public void setDescription() {
+  }
+
+  @Test
+  public void getSize() {
+  }
+
+  @Test
+  public void setSize() {
+  }
+}
